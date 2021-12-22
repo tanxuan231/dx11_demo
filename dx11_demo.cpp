7770e46b0ab9f3ff523db89f5b55b61cc28a9c97
xuan.tan <xuan.tan@shopee.com>
2021-12-22 18:59:24 +0800

    初始化工程，一个黑色的窗口。

---
+++ dx11_demo.cpp
@@ -0,0 +1,33 @@
+﻿#include "systemclass.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    SystemClass* System;
+    bool result;
+
+
+    // Create the system object.
+    System = new SystemClass;
+    if (!System)
+    {
+        return 0;
+    }
+
+    // Initialize and run the system object.
+    result = System->Initialize();
+    if (result)
+    {
+        System->Run();
+    }
+
+    // Shutdown and release the system object.
+    System->Shutdown();
+    delete System;
+    System = 0;
+
+    return 0;
+}
+
