abd2e857ceeafdd65ab0d5aafeaaeb2035a395e2
xuan.tan <xuan.tan@shopee.com>
2021-12-24 15:15:58 +0800

    两个三角形组成一个矩形，渲染texture

--- modelclass.cpp
+++ modelclass.cpp
@@ -88,10 +88,10 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 
 
 	// Set the number of vertices in the vertex array.
-	m_vertexCount = 3;
+	m_vertexCount = 4;
 
 	// Set the number of indices in the index array.
-	m_indexCount = 3;
+	m_indexCount = 6;
 
 	// Create the vertex array.
 	vertices = new VertexType[m_vertexCount];
@@ -111,16 +111,24 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 	vertices[0].position = XMFLOAT3(-1.0f, -1.0f, 0.0f);  // Bottom left.
 	vertices[0].texture = XMFLOAT2(0.0f, 1.0f);
 
-	vertices[1].position = XMFLOAT3(0.0f, 1.0f, 0.0f);  // Top middle.
-	vertices[1].texture = XMFLOAT2(0.5f, 0.0f);
+	vertices[1].position = XMFLOAT3(-1.0f, 1.0f, 0.0f);  // Top left.
+	vertices[1].texture = XMFLOAT2(0.0f, 0.0f);
 
-	vertices[2].position = XMFLOAT3(1.0f, -1.0f, 0.0f);  // Bottom right.
-	vertices[2].texture = XMFLOAT2(1.0f, 1.0f);
+	vertices[2].position = XMFLOAT3(1.0f, 1.0f, 0.0f);  // Top right.
+	vertices[2].texture = XMFLOAT2(1.0f, 0.0f);
+
+	vertices[3].position = XMFLOAT3(1.0f, -1.0f, 0.0f);  // Bottom right.
+	vertices[3].texture = XMFLOAT2(1.0f, 1.0f);
 
 	// Load the index array with data.
+	// 第一个三角形
 	indices[0] = 0;  // Bottom left.
-	indices[1] = 1;  // Top middle.
-	indices[2] = 2;  // Bottom right.
+	indices[1] = 1;  // Top left.
+	indices[2] = 2;  // Top right.
+	// 第二个三角形
+	indices[3] = 0;  // Bottom left.
+	indices[4] = 2;  // Top right.
+	indices[5] = 3;  // Bottom right.
 
 	// Set up the description of the static vertex buffer.
     vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
